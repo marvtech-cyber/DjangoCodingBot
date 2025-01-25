bbef0e20a0780698d8aafc42273a1acb5388fed1
Marvin Nyarenchi <marvin.nyarenchi@github.com>
2025-01-25 11:20:07 +0300

    configured code entry form

--- codingBot/website/views.py
+++ codingBot/website/views.py
@@ -1,6 +1,18 @@
+from django.contrib import messages
 from django.shortcuts import render
 
 
 
 def home(request):
-    return render(request, 'home.html', {})
+    lang_list = ['Objective C', 'R', 'Ruby', 'Swift', 'c++', 'clike', 'css', 'django', 'javascriptc','html', 'markup', 'python', 'rust', 'typesript']
+    if request.method == "POST":
+        code = request.POST['code']
+        lang = request.POST['lang']
+        # Check to make sure they picked a lang
+        if lang == "Select Programming language":
+            messages.success(request, "Hey! You Forgot To Pick A Programming Language.....")
+            return render(request, 'home.html', {'lang_list':lang_list, 'code':code})
+       # return render(request, 'home.html', {'lang_list':lang_list, 'code':code, 'lang':lang})
+
+
+    return render(request, 'home.html', {'lang_list': lang_list})
